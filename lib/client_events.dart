d964f34c891ecdd5838b05ea05323980335c82b7
Da0hn <gabrielhondacba@gmail.com>
2022-05-18 13:20:34 -0400

    feat: add `Client` events

---
+++ lib/client_events.dart
@@ -0,0 +1,17 @@
+import 'client.dart';
+
+abstract class ClientEvent {}
+
+class LoadClientEvent extends ClientEvent {}
+
+class AddClientEvent extends ClientEvent {
+  final Client client;
+
+  AddClientEvent({required this.client});
+}
+
+class RemoveClientEvent extends ClientEvent {
+  final Client client;
+
+  RemoveClientEvent({required this.client});
+}
